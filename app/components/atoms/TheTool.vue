298c31011bda7eb8f03e461113f0528847f95477
Karl <karljoyeux@gmail.com>
2025-04-02 23:09:22 +0200

    initial commit

---
+++ app/components/atoms/TheTool.vue
@@ -0,0 +1,13 @@
+<template >
+    <button class="tool" @click="openWindow">
+        <slot></slot>
+    </button>
+</template>
+<script setup lang="ts">
+
+
+const openWindow = () => window.open("http://localhost:3000/", "secund window", "width=800,height=600,left=100,top=100")
+</script>
+<style lang="css">
+    
+</style>
